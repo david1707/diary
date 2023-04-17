427fe5b38890a26ba7190c9761129eac3f915586
david1707 <DavidAlacant@gmail.com>
2023-04-17 18:24:05 +0200

    Added Entry model and sample entries on the entries page

--- lib/screens/entries_page.dart
+++ lib/screens/entries_page.dart
@@ -1,4 +1,7 @@
+import 'package:diary/utils.dart/extensions.dart';
 import 'package:flutter/material.dart';
+
+import '../models/entry.dart';
 
 class EntriesPage extends StatefulWidget {
   const EntriesPage({super.key});
@@ -8,6 +11,81 @@ class EntriesPage extends StatefulWidget {
 }
 
 class _EntriesPageState extends State<EntriesPage> {
+  List<Entry> entries = [
+    Entry(
+      title: 'Primera entrada',
+      text: 'Açò és el text de la primera entrada',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ),
+    Entry(
+      title: 'Segona entrada',
+      text: 'Açò és el text de la segona entrada',
+      createdAt: DateTime.now().subtract(const Duration(days: 1)),
+      updatedAt: DateTime.now().subtract(const Duration(days: 1)),
+    ),
+    Entry(
+      title: 'Tercera entrada',
+      text: 'Açò és el text de la tercera entrada',
+      createdAt: DateTime.now().subtract(const Duration(days: 2)),
+      updatedAt: DateTime.now().subtract(const Duration(days: 2)),
+    ),
+    Entry(
+      title: 'Quarta entrada',
+      text: 'Açò és el text de la quarta entrada',
+      createdAt: DateTime.now().subtract(const Duration(days: 3)),
+      updatedAt: DateTime.now().subtract(const Duration(days: 3)),
+    ),
+    Entry(
+      title: 'Primera entrada',
+      text: 'Açò és el text de la primera entrada',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ),
+    Entry(
+      title: 'Segona entrada',
+      text: 'Açò és el text de la segona entrada',
+      createdAt: DateTime.now().subtract(const Duration(days: 1)),
+      updatedAt: DateTime.now().subtract(const Duration(days: 1)),
+    ),
+    Entry(
+      title: 'Tercera entrada',
+      text: 'Açò és el text de la tercera entrada',
+      createdAt: DateTime.now().subtract(const Duration(days: 2)),
+      updatedAt: DateTime.now().subtract(const Duration(days: 2)),
+    ),
+    Entry(
+      title: 'Quarta entrada',
+      text: 'Açò és el text de la quarta entrada',
+      createdAt: DateTime.now().subtract(const Duration(days: 3)),
+      updatedAt: DateTime.now().subtract(const Duration(days: 3)),
+    ),
+    Entry(
+      title: 'Primera entrada',
+      text: 'Açò és el text de la primera entrada',
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    ),
+    Entry(
+      title: 'Segona entrada',
+      text: 'Açò és el text de la segona entrada',
+      createdAt: DateTime.now().subtract(const Duration(days: 1)),
+      updatedAt: DateTime.now().subtract(const Duration(days: 1)),
+    ),
+    Entry(
+      title: 'Tercera entrada',
+      text: 'Açò és el text de la tercera entrada',
+      createdAt: DateTime.now().subtract(const Duration(days: 2)),
+      updatedAt: DateTime.now().subtract(const Duration(days: 2)),
+    ),
+    Entry(
+      title: 'Quarta entrada',
+      text: 'Açò és el text de la quarta entrada',
+      createdAt: DateTime.now().subtract(const Duration(days: 3)),
+      updatedAt: DateTime.now().subtract(const Duration(days: 3)),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,8 +104,55 @@ class _EntriesPageState extends State<EntriesPage> {
           ),
         ],
       ),
-      body: Center(
-        child: Column(),
+      body: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Text(
+              'Diary entries',
+              style: TextStyle(
+                fontSize: 40,
+                color: Color(0xffFEFFFF),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: entries.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 4.0),
+                  child: ListTile(
+                    trailing: const Icon(
+                      Icons.read_more,
+                      color: Colors.white,
+                    ),
+                    title: Column(
+                      children: [
+                        Text(
+                          entries[index].title,
+                          style: const TextStyle(
+                            color: Color(0xffFEFFFF),
+                            fontSize: 18,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        Text(
+                          truncateString(entries[index].text, 30),
+                          style: const TextStyle(
+                            color: Color(0xffFEFFFF),
+                            fontSize: 12,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
